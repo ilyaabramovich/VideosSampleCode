c8477c3aec1648fe13c3c3e4fae2217f61e903f8
ilyaabramovich <ilyaabramovich1998@gmail.com>
2026-06-20 17:42:30 +0300

    simplify largest_files script

--- videos/013_finding_your_largest_files_basic_python_recipes/largest_files.py
+++ videos/013_finding_your_largest_files_basic_python_recipes/largest_files.py
@@ -1,32 +1,27 @@
+from heapq import nlargest
 import os
+from pathlib import Path
 import time
 
-from os.path import join, getsize
-from heapq import nlargest
 
-
-def walk_files_and_sizes(start_at: str):
+def walk_files_and_sizes(start_at: Path):
     for root, _, files in os.walk(start_at):
         for file in files:
-            path = join(root, file)
-            try:
-                size = getsize(path)  # bytes
-                yield path, size
-            except OSError:
-                continue
+            path = Path(root) / file
+            if path.is_file():
+                yield path.stat().st_size, path
 
 
 def largest_files(n: int, start_at: str) -> None:
-    MB = 1024 * 1024
-    largest = nlargest(n, walk_files_and_sizes(start_at), key=lambda x: x[1])
+    start_path = Path(start_at)
+    largest = nlargest(n, walk_files_and_sizes(start_path))
 
-    for path, size in largest:
-        print(f'{size//MB} MB {path}')
-
+    for size, path in largest:
+        print(f'{size // (1024 * 1024)} MB {path}')
 
 
 if __name__ == '__main__':
     start = time.perf_counter()
-    largest_files(10, "C:/Users/jtmur/")
+    largest_files(10, str(Path.home()))
     elapsed = time.perf_counter() - start
     print(f'{elapsed} seconds elapsed')
